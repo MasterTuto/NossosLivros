1780ec329feb2b8d0316a438580ce82b5e49e65b
MasterTuto <brenocarvalho2011@gmail.com>
2022-07-10 16:31:23 -0300

    feat: user update

--- users/router.go
+++ users/router.go
@@ -1,11 +1,21 @@
 package users
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
+
+type userUpdatePatch = struct {
+	name          string
+	email         string
+	password      string
+	finishedLoans int
+	loanLimit     int
+}
 
 func createUser(c *gin.Context) {
 	varDb, found := c.Get("db")
@@ -51,7 +61,61 @@ func logUserIn(c *gin.Context) {
 }
 
 func updateUserData(c *gin.Context) {
+	userDb, found := c.Get("db")
+	db := userDb.(*gorm.DB)
 
+	if !found {
+		c.JSON(500, gin.H{"error": "could not get db"})
+		return
+	}
+
+	userId := c.Param("userId")
+
+	var user User
+
+	db.First(&user, userId)
+
+	if (user == User{}) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+		return
+	}
+
+	var requestBody userUpdatePatch = userUpdatePatch{
+		name:          "",
+		email:         "",
+		password:      "",
+		finishedLoans: -1,
+		loanLimit:     -1,
+	}
+
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		c.JSON(400, gin.H{"error": "wrong request body"})
+		return
+	}
+
+	updates := map[string]interface{}{}
+
+	if requestBody.name != "" {
+		updates["name"] = requestBody.name
+	}
+
+	if requestBody.email != "" {
+		updates["email"] = requestBody.email
+	}
+
+	if requestBody.password != "" {
+		updates["password"] = fmt.Sprintf("%x", sha256.Sum256([]byte(requestBody.password)))
+	}
+
+	if requestBody.finishedLoans != -1 {
+		updates["finishedLoans"] = requestBody.finishedLoans
+	}
+
+	if requestBody.loanLimit != -1 {
+		updates["loanLimit"] = requestBody.loanLimit
+	}
+
+	db.Model(&user).Updates(updates)
 }
 
 func getBooksFromUser(c *gin.Context) {
@@ -90,7 +154,7 @@ func UserRoutes(r *gin.Engine) {
 	r.POST("/user/signup", createUser)
 	r.POST("/user/signin", logUserIn)
 
-	r.PATCH("/user", updateUserData)
+	r.PATCH("/user/:userId", updateUserData)
 
 	r.GET("/user/:userId/books", getBooksFromUser)
 	r.PATCH("/user/:userId/books/:bookId", updateUserBook)
